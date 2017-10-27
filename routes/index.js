720b0c968d0108857b8cde69fd043ce59fa1c639
song10 <song10@gmail.com>
2017-10-27 13:02:58 +0800

    add /test

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/test', function (req, res, next) {
+  res.json({ 'test': 'only', 'version': '0.0.1' });
 });
 
 module.exports = router;
